9fcc1fad4e55c6329172a3b87483d0a77cd94752
Ced <ced@MacBook-Pro-de-Ced.local>
2019-11-02 00:11:24 +0100

    mise en place de l'envoie de mail

--- app/src/main/java/com/example/amio_detect/MainActivity.java
+++ app/src/main/java/com/example/amio_detect/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.amio_detect;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -37,5 +42,27 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, switchPref.toString(),
                 Toast.LENGTH_SHORT).show();*/
 
+        sendEmail();
+
+    }
+
+    protected void sendEmail() {
+        Log.i("Send email", "sending mail");
+        SharedPreferences sharedPref =
+                PreferenceManager
+                        .getDefaultSharedPreferences(this);
+
+        try {
+            String to = "";
+            to = sharedPref.getString("your_mail",to);
+            GMailSender sender = new GMailSender("no.reply.amio@gmail.com", "TNCY@2019");
+            sender.sendMail("Test Mail",
+                    "This is Body",
+                    "no.reply.amio@gmail.com",
+                    to);
+            Log.i("SendMail", "mail sent to : " + to + " || ");
+        } catch (Exception e) {
+            Log.e("SendMail", e.getMessage(), e);
+        }
     }
 }
